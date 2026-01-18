c8ab0d7d96d3191f58f20b7a3e0d52e41bb0e108
Marcelo Henrique <marcelohenriquedev12@gmail.com>
2026-01-18 16:51:08 -0300

    Correção push

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [vue(), tailwindcss()],
-
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
-
   base: '/',
-
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     manifest: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/main.ts'),
+    },
   },
-}))
+})
